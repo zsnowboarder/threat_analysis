4449bf11aac5646ec3ebb1bff13e17bc1da950f8
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-12-15 08:43:08 -0800

    Update toxic_transformer.py

--- toxic_transformer.py
+++ toxic_transformer.py
@@ -17,17 +17,19 @@ classifier = pipeline('text-classification', model='unitary/toxic-bert')
 # In[18]:
 
 
-st.title("Behaviour Detection")
+st.title("Threat/Behaviour Detection")
 st.write("")
-st.write("""This excercise uses a combination of supervised and unsupervised approach to solve problems. Supervise learning is applied first to
-classify the toxicity of the text, then unsupervised learning is applied to identify the outliers of the toxicity score.""")
+st.write("""This exercise uses a combination of supervised and unsupervised approaches to analyze the intensity of threat.
+Supervised learning is applied first to classify the toxicity of the text using a pre-trained BERT model (unitary/toxic-bert), then
+unsupervised learning is applied to identify the outliers of the toxicity score using Isolation Forest algorithm.""")
 
 # load data
-#data = pd.read_excel("/mount/src/threat_analysis/data.xlsx")
 data = pd.read_excel("data.xlsx")
+#data = pd.read_excel("/mount/src/threat_analysis/data.xlsx")
+
 #new_text = {"text": "EDP called and told police that he is going to get a gun and shoot people. Police search the area and apprehended EDP to hospital."}
 
-new_text = st.text_area(label="Enter your own text", value="It is a nice day and everything is beautiful. This text will be added to the dataset for analysis. It will not be unusual as there is no risks.")
+new_text = st.text_area(label="Enter your text", value="It is a nice day and everything is beautiful. This text will be added to the dataset for analysis. It will not be unusual as there is no risks.")
 
 if st.button("Analyze"):
     new_text = {"text": new_text}
@@ -52,6 +54,7 @@ if st.button("Analyze"):
     data["analysis"] = data["analysis"].map({-1:"Unusual", 1:""})
     data = data.drop(columns=["score"])
     st.table(data)
+
 
 
 # In[ ]:
